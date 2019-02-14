7b55645e534c6c6f25bafba6727959a80b46d443
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-02-14 12:34:27 +0100

    Removed object files.

--- game/hunt/huntd/ctl.d
+++
@@ -1,1 +0,0 @@
-hunt/huntd/ctl.o hunt/huntd/ctl.d: hunt/huntd/ctl.c /usr/include/stdc-predef.h hunt/huntd/bsd.h
--- game/hunt/huntd/ctl_transact.d
+++
@@ -1,2 +0,0 @@
-hunt/huntd/ctl_transact.o hunt/huntd/ctl_transact.d: hunt/huntd/ctl_transact.c /usr/include/stdc-predef.h \
- hunt/huntd/bsd.h
